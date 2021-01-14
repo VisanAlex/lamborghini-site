29ba365f0ebc5c59f5e041e2312d3dc6648e24d2
Alex Visan <visanpaypal@gmail.com>
2021-01-14 01:02:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, instances);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, instances);
+  });
+
+
+  
+
+let tabs = document.querySelector('.tab');
+let link = document.querySelector('.link')
+window.addEventListener("DOMContentLoaded", event => {
+  const options = {
+    duration: 300,
+    onShow: null,
+    swipeable: false,
+    responsiveThreshold: Infinity
+  };
+
+  const tabsContainer = document.querySelector(".tabs");
+  M.Tabs.init(tabsContainer, options);
+});
+
+
